680575971005912c714f23e0a33b084f72cfbeb5
boooiil <boooiil@outlook.com>
2023-11-07 01:32:06 -0600

    handle SIGINT

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Container } from 'application/Container'
 import { Ticker } from 'application/Ticker'
+
 /**
  * Because we are using hardware specific encoders,
  * we will need to get whether the user wants to use these encoders.
@@ -10,9 +11,9 @@ import { Ticker } from 'application/Ticker'
 /**
  * WALK AWAY NOTES (for when you return to this eventually):
  * 
- * 1. There is an issue with the converted_resolution, use the redesign.js as a reference to the correct values.
- * 2. Still need to add conversion child process.
- * 3. Make sure that the conversion process accounts for encoder/decoder failures and obtains new ffmpeg arguments.
+ * Do validation process.
+ * Refine encoding fallback process.
+ * Refine error handling in conversion process.
  * 
  * 
  * 
@@ -31,7 +32,12 @@ async function main() {
 
     new Ticker(container).startProcess()
 
-    console.log(container)
+    // handle pressing ctrl+c
+    process.on('SIGINT', () => {
+        // do something
+        console.log('RECEIVED SIGINT')
+        process.exit()
+    })
 
 }
 
